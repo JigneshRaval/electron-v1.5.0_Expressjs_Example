d2325aefcceb61502ac7724f3741a0b216f8afbe
jignesh.rawal <jignesh.rawal@bitbucket.aon.com>
2017-01-31 16:40:42 +0530

    Electron 1.5.0 with Express.js
    Electron 1.5.0 with Express.js

---
+++ main.js
@@ -0,0 +1,134 @@
+// main.js
+
+/**
+ * How to run Express.js application Electron
+ *
+ * 1. Call your server.js file here which contains code for Express.js application
+ * 		app.server = require(__dirname + '/location of your server.js/file')();
+ *
+ * 2. Load URL in following format like  :	win.loadURL('http://localhost:'+3000);
+*/
+
+const {app, BrowserWindow} = require('electron');
+
+fs = require('fs');
+//config = JSON.parse(fs.readFileSync('config.json'));
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+	
+	// Instantiate Express App
+	// 1. Call your server.js file here which contains code for Express.js application
+	app.server = require(__dirname + '/server')();
+	
+	// Create the browser window.
+	win = new BrowserWindow();
+	win.maximize();
+	
+	// and load the index.html of the app.
+	// 2. Load URL in following format
+	win.loadURL('http://localhost:'+3000);
+	
+	
+	// Open the DevTools.
+	// win.webContents.openDevTools();
+	
+	win.focus();
+	
+	// Emitted when the window is closed.
+	win.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null
+	});
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+})
+
+app.on('activate', () => {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (win === null) {
+		createWindow();
+	}
+})
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+/* OLD CODE */
+/*
+	const {app, BrowserWindow} = require('electron')
+	const path = require('path')
+	const url = require('url')
+	
+	// Keep a global reference of the window object, if you don't, the window will
+	// be closed automatically when the JavaScript object is garbage collected.
+	let win
+	console.log("win :", win);
+	
+	function createWindow () {
+	// Create the browser window.
+	win = new BrowserWindow({width: 800, height: 600, frame: false})
+	
+	// and load the index.html of the app.
+	win.loadURL(url.format({
+	pathname: path.join(__dirname, 'app/components/snippets/index.html'),
+	protocol: 'file:',
+	slashes: true
+	}))
+	
+	// Open the DevTools.
+	win.webContents.openDevTools()
+	
+	// Emitted when the window is closed.
+	win.on('closed', () => {
+	// Dereference the window object, usually you would store windows
+	// in an array if your app supports multi windows, this is the time
+	// when you should delete the corresponding element.
+	win = null
+	})
+	}
+	
+	// This method will be called when Electron has finished
+	// initialization and is ready to create browser windows.
+	// Some APIs can only be used after this event occurs.
+	app.on('ready', createWindow)
+	
+	// Quit when all windows are closed.
+	app.on('window-all-closed', () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+	app.quit()
+	}
+	})
+	
+	app.on('activate', () => {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (win === null) {
+	createWindow()
+	}
+	})
+	
+	// In this file you can include the rest of your app's specific main process
+	// code. You can also put them in separate files and require them here.	
+*/
